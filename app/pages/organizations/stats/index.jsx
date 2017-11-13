36b155f260502ee58ba26b0d0b627a08f4bfe6f6
mcbouslog <mcbouslog@gmail.com>
2017-11-13 10:47:13 -0600

    add stats folder

---
+++ app/pages/organizations/stats/index.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import qs from 'qs';
+import { ProjectStatsPage } from './stats.jsx';
+import getWorkflowsInOrder from '../../../lib/get-workflows-in-order.coffee';
+
+class ProjectStatsPageController extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleGraphChange = this.handleGraphChange.bind(this);
+    this.handleWorkflowChange = this.handleWorkflowChange.bind(this);
+    this.handleRangeChange = this.handleRangeChange.bind(this);
+    this.getWorkflows = this.getWorkflows.bind(this);
+
+    this.state = {
+      workflowList: []
+    };
+  }
+
+  componentDidMount() {
+    this.getWorkflows(this.props.organization);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.organization !== nextProps.organization) {
+      this.getWorkflows(nextProps.organization);
+    }
+  }
+
+  getWorkflows(organization) {
+    const fields = [
+      'active',
+      'classifications_count',
+      'completeness',
+      'configuration',
+      'display_name',
+      'retired_set_member_subjects_count',
+      'retirement,subjects_count'
+    ];
+
+    organization.get('projects')
+      .then((projects) => {
+        projects.map((project) => {
+          const query = {
+            fields: fields.join(','),
+            page_size: project.links.workflows.length
+          };
+
+          getWorkflowsInOrder(project, query)
+            .then((workflows) => {
+              const workflowsSetToBeVisible =
+                workflows.filter((workflow) => {
+                  let statsVisible = workflow.active;
+                  if (workflow.configuration.stats_hidden !== undefined) {
+                    statsVisible = !workflow.configuration.stats_hidden;
+                  }
+                  return (statsVisible ? workflow : null);
+                });
+              const workflowList = this.state.workflowList.concat(workflowsSetToBeVisible);
+              this.setState({ workflowList });
+            });
+        });
+      });
+  }
+
+  getQuery(which) {
+    return qs.parse(location.search.slice(1))[which];
+  }
+
+  handleGraphChange(which, e) {
+    const query = qs.parse(location.search.slice(1));
+    query[which] = e.target.value;
+    query[`${which}Range`] = undefined;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
+  }
+
+  handleWorkflowChange(which, e) {
+    const query = qs.parse(location.search.slice(1));
+    const [nameChange, value] = e.target.value.split('=');
+    if (nameChange === 'workflow_id') {
+      query[nameChange] = value;
+    } else {
+      query.workflow_id = undefined;
+    }
+    query[`${which}Range`] = undefined;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
+  }
+
+  handleRangeChange(which, range) {
+    const query = qs.parse(location.search.slice(1));
+    query[`${which}Range`] = range;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
+  }
+
+  render() {
+    const queryProps = {
+      handleGraphChange: this.handleGraphChange,
+      handleRangeChange: this.handleRangeChange,
+      handleWorkflowChange: this.handleWorkflowChange,
+      classificationsBy: this.getQuery('classification') || 'day',
+      classificationRange: this.getQuery('classificationRange'),
+      commentsBy: this.getQuery('comment') || 'day',
+      commentRange: this.getQuery('commentRange'),
+      projectId: this.props.organization.id,
+      workflowId: this.getQuery('workflow_id'),
+      totalVolunteers: 123,
+      currentClassifications: 456,
+      workflows: this.state.workflowList,
+      startDate: this.props.organization.listed_date
+    };
+
+    return <ProjectStatsPage {...queryProps} />;
+  }
+}
+
+ProjectStatsPageController.contextTypes = { router: React.PropTypes.object };
+
+ProjectStatsPageController.propTypes = {
+  organization: React.PropTypes.object,
+  params: React.PropTypes.object
+};
+
+module.exports = ProjectStatsPageController;
