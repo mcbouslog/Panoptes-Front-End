90d3cddf6c3f16a99c403cd73241c2f30a7da4d6
mcbouslog <mcbouslog@gmail.com>
2018-01-12 16:21:24 -0600

    init stats add

--- app/pages/organization/stats/index.jsx
+++ app/pages/organization/stats/index.jsx
@@ -1,13 +1,99 @@
 import React from 'react';
+import qs from 'qs';
 import ProjectNavbar from '../../project/project-navbar';
+import { ProjectStatsPage } from '../../project/stats/stats.jsx';
+import getWorkflowsInOrder from '../../../lib/get-workflows-in-order.coffee';
 
 class OrganizationStatsController extends React.Component {
   constructor() {
     super();
+
+    this.handleGraphChange = this.handleGraphChange.bind(this);
+    this.handleWorkflowChange = this.handleWorkflowChange.bind(this);
+    this.handleRangeChange = this.handleRangeChange.bind(this);
+    this.getWorkflows = this.getWorkflows.bind(this);
+
+    this.state = {
+      workflowList: []
+    };
   }
 
   componentDidMount() {
-    console.log('Hello are you there?');
+    this.getWorkflows(this.props.organization);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.organization !== nextProps.organization) {
+      this.getWorkflows(nextProps.organization);
+    }
+  }
+
+  getWorkflows(organization) {
+    const fields = [
+      'active',
+      'classifications_count',
+      'completeness',
+      'configuration',
+      'display_name',
+      'retired_set_member_subjects_count',
+      'retirement,subjects_count'
+    ];
+
+    organization.get('projects')
+      .then((projects) => {
+        projects.map((project) => {
+          const query = {
+            fields: fields.join(','),
+            page_size: project.links.workflows.length
+          };
+
+          getWorkflowsInOrder(project, query)
+            .then((workflows) => {
+              const workflowsSetToBeVisible =
+                workflows.filter((workflow) => {
+                  let statsVisible = workflow.active;
+                  if (workflow.configuration.stats_hidden !== undefined) {
+                    statsVisible = !workflow.configuration.stats_hidden;
+                  }
+                  return (statsVisible ? workflow : null);
+                });
+              const workflowList = this.state.workflowList.concat(workflowsSetToBeVisible);
+              this.setState({ workflowList });
+            });
+        });
+      });
+  }
+
+  getQuery(which) {
+    return qs.parse(location.search.slice(1))[which];
+  }
+
+  handleGraphChange(which, e) {
+    const query = qs.parse(location.search.slice(1));
+    query[which] = e.target.value;
+    query[`${which}Range`] = undefined;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
+  }
+
+  handleWorkflowChange(which, e) {
+    const query = qs.parse(location.search.slice(1));
+    const [nameChange, value] = e.target.value.split('=');
+    if (nameChange === 'workflow_id') {
+      query[nameChange] = value;
+    } else {
+      query.workflow_id = undefined;
+    }
+    query[`${which}Range`] = undefined;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
+  }
+
+  handleRangeChange(which, range) {
+    const query = qs.parse(location.search.slice(1));
+    query[`${which}Range`] = range;
+    const { owner, name } = this.props.params;
+    this.context.router.replace({ pathname: `/projects/${owner}/${name}/stats/`, query });
   }
 
   render() {
@@ -28,9 +114,26 @@ class OrganizationStatsController extends React.Component {
         projectAvatar={this.props.organizationAvatar}
       />);
 
+    const queryProps = {
+      handleGraphChange: this.handleGraphChange,
+      handleRangeChange: this.handleRangeChange,
+      handleWorkflowChange: this.handleWorkflowChange,
+      classificationsBy: this.getQuery('classification') || 'day',
+      classificationRange: this.getQuery('classificationRange'),
+      commentsBy: this.getQuery('comment') || 'day',
+      commentRange: this.getQuery('commentRange'),
+      projectId: this.props.organization.id,
+      workflowId: this.getQuery('workflow_id'),
+      totalVolunteers: 123,
+      currentClassifications: 456,
+      workflows: this.state.workflowList,
+      startDate: this.props.organization.listed_date
+    };
+
     return (
       <div className="project-page project-background" style={backgroundStyle}>
         {navbar}
+        <ProjectStatsPage {...queryProps} />
       </div>);
   }
 }
